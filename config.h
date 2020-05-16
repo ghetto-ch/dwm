5062b1a8b98e5956b9247b3956e80686e054fadc
ghetto <ghetto.ch@gmail.com>
2020-05-16 21:10:30 +0200

    systray

--- config.h
+++ config.h
@@ -1,8 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
+<<<<<<< HEAD
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+=======
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+>>>>>>> 1fcf589... systray
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -12,21 +21,27 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+<<<<<<< HEAD
 static const char col_green[]       = "#229922";
 static const char col_red[]         = "#BB2222";
 static const char col_blue[]        = "#1111BB";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
+=======
+>>>>>>> 1fcf589... systray
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
+<<<<<<< HEAD
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
+=======
+>>>>>>> 1fcf589... systray
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -38,9 +53,14 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+<<<<<<< HEAD
 	{ "Qalculate-gtk",     NULL,       NULL,       0,            0,           1 },
 	{ "Microsoft Teams - Preview",     NULL,       NULL,       0,            1,           -1 },
 	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
+=======
+	{ "Qalculate-gtk",  NULL,       NULL,       0,       1,           -1 },
+	{ "Microsoft Teams - Preview",     NULL,       NULL,       0,            1,           -1 },
+>>>>>>> 1fcf589... systray
 };
 
 /* layout(s) */
@@ -68,9 +88,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+<<<<<<< HEAD
 static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 /* static const char *termcmd[]  = { "st", NULL }; */
 static const char *favcmd[]   = { "dmenu-favorites", NULL};
+=======
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", NULL };
+>>>>>>> 1fcf589... systray
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -107,8 +132,12 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+<<<<<<< HEAD
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} },
 	{ MODKEY,                       XK_v,      spawn,          {.v = favcmd } },
+=======
+	{ MODKEY|ShiftMask,				XK_r,      quit,           {1} },
+>>>>>>> 1fcf589... systray
 };
 
 /* button definitions */
@@ -118,7 +147,11 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+<<<<<<< HEAD
 	/* { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } }, */
+=======
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+>>>>>>> 1fcf589... systray
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
